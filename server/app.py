941ed9e0257189f1e38a130f318e722fd6e7a4e5
Halim Choi <hchoicareer@gmail.com>
2023-06-01 12:40:25 -0400

    completed mock cc

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Activity, Camper
+from models import db, Activity, Camper, Signup
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
@@ -22,6 +22,73 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
+
+@app.route('/campers', methods=['GET', 'POST'])
+def campers():
+    if request.method == 'GET':
+        campers = Camper.query.all()
+        serialized_campers = [camper.to_dict(rules=('-activities', '-signups')) for camper in campers]
+        for dicts in serialized_campers:
+            dicts.pop('created_at')
+            dicts.pop('updated_at')
+        return serialized_campers
+    elif request.method == 'POST':
+        new_camper = Camper(
+            name = request.get_json()["name"],
+            age = request.get_json()["age"]
+        )
+        db.session.add(new_camper)
+        db.session.commit()
+
+        return make_response(jsonify(new_camper.to_dict()), 201)
+    return {'error': '400: Validation Error'}, 400
+
+@app.route('/campers/<int:id>')
+def camper_by_id(id):
+    camper = Camper.query.filter(Camper.id == id).first()
+    if camper:
+        serialized_camper = camper.to_dict()
+        serialized_camper.pop('created_at')
+        serialized_camper.pop('updated_at')
+        serialized_camper.update({"activities": serialized_camper["signups"]})
+        serialized_camper.pop('signups')
+        return make_response(jsonify(serialized_camper), 200)
+    return {'error': '404: Camper not found'}, 404
+
+@app.route('/activities')
+def activities():
+    activities = Activity.query.all()
+    serialized_acts = [activity.to_dict() for activity in activities]
+    for acts in serialized_acts:
+        acts.pop('created_at')
+        acts.pop('updated_at')
+        acts.pop('signups')
+    response = make_response(jsonify(serialized_acts), 200)
+    return response
+
+@app.route('/activities/<int:id>', methods=["DELETE"])
+def delete_activity(id):
+    activity = Activity.query.filter(Activity.id == id).first()
+    if activity:
+        db.session.delete(activity)
+        db.session.commit()
+
+        return make_response('', 204)
+    
+    return "404: Activity Not Found", 404
+
+@app.route('/signups', methods=['POST'])
+def create_signup():
+    new_signup = Signup(
+        time = request.get_json()["time"],
+        camper_id = request.get_json()["camper_id"],
+        activity_id = request.get_json()["activity_id"]
+    )
+    if new_signup:
+        db.session.add(new_signup)
+        db.session.commit()
+        return make_response(jsonify(new_signup.to_dict()), 201)
+    return {'error': '400: Validation Error'}, 400
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from flask import Flask, abort
 
 convention = {
   "ix": "ix_%(column_0_label)s",
@@ -19,9 +20,16 @@ db = SQLAlchemy(metadata=metadata)
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
 
+    serialize_rules = ('-signups.activity', '-campers.activities')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    signups = db.relationship('Signup', backref='activity')
+    campers = association_proxy('signups', 'camper', creator=lambda cmp: Signup(camper=cmp))
 
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -29,17 +37,50 @@ class Activity(db.Model, SerializerMixin):
 class Camper(db.Model, SerializerMixin):
     __tablename__ = 'campers'
 
+    serialize_rules = ('-signups.camper', '-activities.campers')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    signups = db.relationship('Signup', backref='camper')
+    activities = association_proxy('signups', 'activity', creator=lambda act: Signup(activity=act))
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            abort(422, 'Camper must have a name.')
+        return name
+    
+    @validates('age')
+    def validate_age(self, key, age):
+        if 8 <= age <= 18:
+            return age
+        abort(422, 'Camper must have an age between 8 and 18.')
+    
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
     
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
+    serialize_rules = ('-activity.signups', '-camper.signups')
+
     id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+
+    @validates('time')
+    def validate_time(self, key, time):
+        if 0 <= time <= 23:
+            return time
+        abort(422, 'Signup must have a time between 0 and 23')
 
     def __repr__(self):
         return f'<Signup {self.id}>'
